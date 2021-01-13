274d897ef62c75595d679c55005ebc00b1e1f1a8
0hana <0hana@users.noreply.github.com>
2021-01-13 14:08:51 -0800

    Adds S and Z constants
    Changes ternary_relation to ternary_greater
    Changes s_ternary_relation to S_Greater and z_ternary_greater to Z_Greater which take 2 (v c * c) and cast internally

--- common.c
+++ common.c
@@ -1,10 +1,12 @@
 //Zero Hanami (C) 2020
 #include "common.h"
 
-enum ternary s_ternary_relation(s c * c S1, s c * c S2) { return *S1 < *S2 ? ternary_2 : *S1 > *S2 ? ternary_1 : ternary_0; }
+s c S = sizeof(s)*8;
+z c Z = ~((s)-1/2);
 
-enum ternary z_ternary_relation(z c * c Z1, z c * c Z2) { return *Z1 < *Z2 ? ternary_2 : *Z1 > *Z2 ? ternary_1 : ternary_0; }
+enum ternary S_Greater(v c * c S1, v c * c S2) { return *(s c * c)S1 < *(s c * c)S2 ? ternary_2 : *(s c * c)S1 > *(s c * c)S2 ? ternary_1 : ternary_0; }
+enum ternary Z_Greater(v c * c Z1, v c * c Z2) { return *(z c * c)Z1 < *(z c * c)Z2 ? ternary_2 : *(z c * c)Z1 > *(z c * c)Z2 ? ternary_1 : ternary_0; }
 
-v copy(s Size, v * c O1, v c * c O2) { while(Size-- > 0) { ((u*)O1)[Size] = ((u*)O2)[Size]; } }
+v Copy(s Size, v * c O1, v c * c O2) { while(Size-- > 0) { ((u*)O1)[Size] = ((u*)O2)[Size]; } }
 
-v swap(s Size, v * c O1, v * c O2) { while(Size-- > 0) { u O = ((u*)O1)[Size]; ((u*)O1)[Size] = ((u*)O2)[Size]; ((u*)O2)[Size] = O; } }
+v Swap(s Size, v * c O1, v * c O2) { while(Size-- > 0) { u c O = ((u*)O1)[Size]; ((u*)O1)[Size] = ((u*)O2)[Size]; ((u*)O2)[Size] = O; } }
--- common.h
+++ common.h
@@ -2,29 +2,33 @@
 #ifndef common_h
 #define common_h
 
+#include <stdlib.h>
+/*
+	For types size_t and ssize_t, as well as dynamic memory allocation functions
+	malloc(), realloc(), and free()
+*/
 #ifdef DEBUG
 #include <assert.h>
+#include <stdio.h>
+#define _Print_Error_(TEXT, ...) fprintf(stderr, TEXT "\n" __VA_OPT__(,) __VA_ARGS__)
 #endif
 
-#include <stdlib.h>
+#define c const
+typedef void v;
+typedef char u;
+typedef size_t s; extern s c S;//S is the log2 of the s state space
+typedef ssize_t z; extern z c Z;//Z is the z cast of the largest s value that multiplied by 2 is 0 (or the s state space divided by 2)
 
 enum binary { binary_0 = 0, binary_1 = ~0 };
+typedef enum ternary { ternary_0 = 0, ternary_1 = +1, ternary_2 = -1 } (*ternary_greater)(v c * c, v c * c);
+/*
+	ternary_greater functions cast to the appropriately typed pointers internally, and returns whichever value is deemed greater in order statistic value.
+	The order is determined by the function, thus in sorting an array of unknown type, this function determines the type and ordering among items within.
+*/
+enum ternary S_Greater(v c * c S1, v c * c S2);//Casts to (s c * c) to dereference internally
+enum ternary Z_Greater(v c * c Z1, v c * c Z2);//Casts to (z c * c) to dereference internally
 
-#define c const
-#define s size_t
-#define u char
-#define v void
-#define z ssize_t
+v Copy(s Size, v * c O1, v c * c O2);
+v Swap(s Size, v * c O1, v * c O2);
 
-typedef enum ternary { ternary_0 = 0, ternary_1 = +1, ternary_2 = -1 } (*ternary_relation)(v c * c, v c * c);
-
-enum ternary s_ternary_relation(s c * c, s c * c);
-
-enum ternary z_ternary_relation(z c * c, z c * c);
-
-v copy(s, v * c, v c * c);
-
-v swap(s, v * c, v * c);
-
-#define IMPLY(A, B) (!(A) || (B))
 #endif//common_h
