4d5484e6313ac5439ba7e1b03ddb327bb829260c
0hana <0hana@users.noreply.github.com>
2021-02-09 22:45:33 -0800

    Refactors graph struct, _transpose, & _free to improve semantic clarity
    graph_bfs and dfs commented out for refactor and imp respectively

--- graph.c
+++ graph.c
@@ -1,3 +1,66 @@
+//Zero Hanami (C) 2021: graph.c
+
+#include "graph.h"
+/* graph construct
+
+struct graph {
+	size_t const Vertices;
+	struct graph_vertex {
+		size_t const Edges;
+		struct graph_vertex_edge {
+			size_t const Destination;
+			float Weight;
+		} * const Edge;
+	} * const Vertex;
+};
+
+*/
+
+struct graph * graph_transpose(struct graph const * const G) {
+	//Allocate return structures
+	struct graph * Transpose = malloc(sizeof(struct graph));
+	if(!Transpose) return NULL;
+	*(struct graph_vertex **)&Transpose->Vertex = malloc(sizeof(struct graph_vertex) * G->Vertices);
+	if(!Transpose->Vertex) return free(Transpose), NULL;
+	else {
+		//Determine number of edges per transpose vertex
+		for(size_t V = 0; V < G->Vertices; V++) *(size_t *)Transpose->Vertex[V].Edges = 0;
+		for(size_t V = 0; V < G->Vertices; V++) for(size_t E = 0; E < G->Vertex[V].Edges; E++) (*(size_t *)&Transpose->Vertex[G->Vertex[V].Edge[E].Destination].Edges)++;
+		//Allocate edge array for each transpose vertex
+		for(size_t V = 0; V < G->Vertices; V++) {
+			*(struct graph_vertex_edge **)&Transpose->Vertex[V].Edge = malloc(sizeof(struct graph_vertex_edge) * Transpose->Vertex[V].Edges);
+			if(!Transpose->Vertex[V].Edge) {
+				//Release all allocations prior to malloc failure
+				while(V-- > 0) free(Transpose->Vertex[V].Edge);
+				return free(Transpose->Vertex), free(Transpose), NULL;
+			}
+		}
+	}
+
+	//Translate G to Transpose
+	*(size_t *)&Transpose->Vertices = G->Vertices;
+	{
+		size_t Adjacent[G->Vertices];//Counter for each transpose vertex
+		for(size_t V = 0; V < G->Vertices; V++) Adjacent[V] = 0;
+		for(size_t V = 0; V < G->Vertices; V++) {
+			for(size_t E = 0; V < G->Vertex[V].Edges; E++) {
+				#define X G->Vertex[V].Edge[E]
+				*(size_t *)&Transpose->Vertex[X.Destination].Edge[Adjacent[X.Destination]].Destination = V;
+				Transpose->Vertex[X.Destination].Edge[Adjacent[X.Destination]++].Weight = X.Weight;
+				#undef X
+			}
+		}
+	}
+	return Transpose;
+}
+
+void graph_free(struct graph * const G) {
+	for(size_t V = 0; V < G->Vertices; V++) free(G->Vertex[V].Edge);
+	free(G->Vertex);
+	free(G);
+}
+
+/*
 //Zero Hanami (C) 2021
 
 #include "graph.h"
@@ -45,3 +108,4 @@ struct graph_bfs_data const * graph_bfs(struct graph * const G, size_t const Sou
 
 struct graph_dfs_data const * graph_dfs(struct graph * const G, size_t Source) {
 }
+*/
--- graph.h
+++ graph.h
@@ -1,65 +1,22 @@
-//Zero Hanami (C) 2021
+//Zero Hanami (C) 2021: graph.h
+
 #ifndef _GRAPH
 #define _GRAPH
-
 #include <stdlib.h>
 
 struct graph {
 	size_t const Vertices;
-	size_t const * const Adjacencies;
-	struct graph_edge {
-		float Weight;
-		size_t Destination;
-	} const * const * const Edge;
-} * graph_transpose(struct graph * G) {
-	//Allocate return structures
-	struct graph * Transpose = malloc(sizeof(struct graph));
-	if(!Transpose) return NULL;
-	size_t * Adjacencies = malloc(sizeof(size_t) * G->Vertices);
-	if(!Adjacencies) return free(Transpose), NULL;
-	else {
-		//Determine Adjacencies for each vertex in Transpose;
-		for(size_t V = 0; V < G->Vertices; V++) Adjacencies[V] = 0;
-		for(size_t V = 0; V < G->Vertices; V++) {
-			for(size_t Adjacent = 0; Adjacent < G->Adjacencies[V]; Adjacent++) {
-				Adjacencies[G->Edge[V][Adjacent].Destination]++;
-			}
-		}
-	}
-	struct graph_edge ** Edge = malloc(sizeof(struct graph_edge *) * G->Vertices);
-	if(!Edge) return free(Adjacencies), free(Transpose), NULL;
-	else {
-		for(size_t V = 0; V < G->Vertices; V++) {
-			Edge[V] = malloc(sizeof(struct graph_edge) * Adjacencies[V]);
-			if(!Edge[V]) {
-				while(V-- != (size_t)-1) free(Edge[V]);
-				return free(Edge), free(Adjacencies), free(Transpose), NULL;
-			}
-		}
-	}
+	struct graph_vertex {
+		size_t const Edges;
+		struct graph_vertex_edge {
+			size_t const Destination;
+			float Weight;
+		} * const Edge;
+	} * const Vertex;
+} * graph_transpose(struct graph const * const G)
+, * graph_via_CLI(void);
 
-	//Initialize return structures
-	*(size_t *)&Transpose->Vertices = G->Vertices;
-	*(size_t **)&Transpose->Adjacencies = Adjacencies;
-	*(struct graph_edge ***)&Transpose->Edge = Edge;
-	{
-		size_t Adjacency[G->Vertices];
-		for(size_t V = 0; V < G->Vertices; V++) Adjacency[V] = 0;
-		for(size_t V = 0; V < G->Vertices; V++) {
-			for(size_t Adjacent = 0; Adjacent < G->Adjacencies[V]; Adjacent++) {
-				*(struct graph_edge *)&Transpose->Edge[G->Edge[V][Adjacent].Destination][Adjacency[G->Edge[V][Adjacent].Destination]++] = (struct graph_edge){ .Destination = V, .Weight = G->Edge[V][Adjacent].Weight };
-			}
-		}
-	}
-	return Transpose;	
-}
-
-void graph_free(struct graph * const G) {
-	for(size_t V = 0; V < G->Vertices; V++) free(G->Edge[V]);
-	free(G->Adjacencies);
-	free(G->Edge);
-	free(G);
-}
+void graph_free(struct graph * const G);
 
 /*
 struct graph_bfs_data {
@@ -77,4 +34,5 @@ struct graph_dfs_data {
 	size_t const Topological_Order[];
 } const * graph_dfs(struct graph * const G, size_t Source);
 */
+
 #endif//_GRAPH
