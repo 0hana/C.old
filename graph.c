040de9ea9bb2288c12cb46bf796c341b360c130f
0hana <0hana@users.noreply.github.com>
2021-02-14 14:14:34 -0800

    Removes failure check in graph_random on random_array_of_unique_indices
    random_array_of_unique_indices should not fail here unless available stack memory is insufficient --
    thus if it crashes here, I believe the user of graph_random has bigger problems.
    
    It is worth noting that graph_random is still untested, though there has been preliminary testing
    on random_array_of_unique_indices and it appears to work as expected: its former issue of outcome
    bias appears to be resolved via the card drawing method, though a cheaper spacial solution is
    being looked into.

--- graph.c
+++ graph.c
@@ -73,7 +73,10 @@ struct graph * graph_random(size_t const Vertex_Limit, size_t const Edge_Limit) 
 			}
 			//Generate the random edge destinations in ascending order
 			size_t Random_Destination[Random->Vertex[V].Edges], Minimum = 0;
-			if(!random_array_of_unique_indices(Random_Destination, Random->Vertices - 1, Random->Vertex[V].Edges)) return graph_free(Random), NULL;
+
+			random_array_of_unique_indices(Random_Destination, Random->Vertices - 1, Random->Vertex[V].Edges);
+			//function will only fail on insufficient stack memory -- requires Random->Vertex[V].Edges * sizeof(size_t) + constant to run successfully
+
 			for(size_t E = 0; E < Random->Vertex[V].Edges; E++) if((Random_Destination[E] + V + 1) % Random->Vertices < (Random_Destination[Minimum] + V + 1) % Random->Vertices) Minimum = E;
 			for(size_t E = 0; E < Random->Vertex[V].Edges; E++) {
 				*(size_t *)&Random->Vertex[V].Edge[E].Destination = (Random_Destination[(Minimum + E) % Random->Vertex[V].Edges] + V + 1) % Random->Vertices;
